3cab6ec57074168514a9b2b18de61e76232614b1
abhinavkorpal <abhinavkorpaljv@gmail.com>
2017-07-05 17:33:26 +0530

    Amazon

--- amazon.sh
+++ amazon.sh
@@ -1,22 +1,7 @@
 #!/bin/bash
 
-echo "Are you going to commit if yes type 1 or else"
-read text
-
-echo "Enter remote branch: "
-read branch
-
-echo "Enter commit description: "
-read  desc
-if  [ text == 1 ]
-then
+    git init
     git add .
-    git commit -m $desc
-    git checkout $branch
-    git status
-    git push
-    git status
-
-else
-    echo "No Issue Nect Time !!!"
-fi
+    git checkout master
+    git commit -m  Amazon
+    git push https://github.com/abhinavkorpal/Amazon-Web-Services-Doc.git
